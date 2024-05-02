cc45a80df3a24e26b880dbc6d4ad74a56c8214ff
flacarpanpit <116425747+flacarpanpit@users.noreply.github.com>
2024-05-02 16:12:56 +0200

    Create api.php

---
+++ api.php
@@ -0,0 +1,29 @@
+<?php
+include_once 'db.php';
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if ($_SERVER['PATH_INFO'] == '/movies') {
+      if (isset($_GET['titolo'])) {
+            echo json_encode(get_film($_GET['titolo'])); }  
+      else {
+            echo json_encode(get_film(null)); }
+    } elseif ($_SERVER['PATH_INFO'] == '/actors') {
+        if (isset($_GET['attore'])) {
+            echo json_encode(get_attori($_GET['attore']));  }
+        else {
+            echo json_encode(get_attori(null)); }
+    } elseif ($_SERVER['PATH_INFO'] == '/genres') {
+        if (isset($_GET['genere'])) {
+            echo json_encode(get_generi($_GET['genere']));
+      } else {
+        echo json_encode(get_generi(null));
+      }
+    } elseif ($_SERVER['PATH_INFO'] == '/directors') {
+        if (isset($_GET['regista'])) {
+            echo json_encode(get_registi($_GET['regista']));
+        } else {
+            echo json_encode(get_registi(null));
+        }
+    }
+} else {
+    http_response_code(404);
+}
